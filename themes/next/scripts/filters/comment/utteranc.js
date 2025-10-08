416cf47b58326c9a9699c335a357794d0243c324
李文坤 <Lwenkun666@gmail.com>
2025-10-09 03:00:49 +0800

    Update utteranc.js

--- themes/next/scripts/filters/comment/utteranc.js
+++ themes/next/scripts/filters/comment/utteranc.js
@@ -8,7 +8,7 @@ hexo.extend.filter.register('theme_inject', injects => {
   if (!theme.utteranc.enable) return;
 
   injects.comment.raw('gitalk', '<script src="https://utteranc.es/client.js" \
-                                    repo="Lwenkun/lwenkun.github.io" \
+                                    repo="liwenkun/liwenkun.github.io" \
                                     issue-term="pathname" \
                                     theme="github-dark" \
                                     crossorigin="anonymous" \
